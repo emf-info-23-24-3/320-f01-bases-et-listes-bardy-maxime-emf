c382303f0034a82eaedcb4d3abe79988ca9a72a7
Maxime Bardy <maxime.bardy@studentfr.ch>
2024-09-04 10:16:15 +0200

    v2

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,16 +1,23 @@
 package app;
 
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
+
+        ArrayList<Personne> personnes = new ArrayList<>();
 
         Personne personne1 = new Personne("Valentin", "Gremaud", "teste", 1784, 92);
         Personne personne2 = new Personne("Michu", "Grem", "te", 1765, 45);
         Personne personne3 = new Personne("Gui", "Gre", "testeeeee", 1829, 32);
 
+        personnes.add(personne3);
+        personnes.add(personne1);
+        personnes.add(personne2);
 
-        for (Personne personne : ) {
-            System.out.println();
+        for (Personne personne : personnes) {
+            System.out.println(personne.getNom());
         }
     }
 
